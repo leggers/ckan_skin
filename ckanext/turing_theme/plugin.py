bc02b5d9b78d13bfb875b231ae7322e381e33c3c
Lucas S Eggers <l.eggers91@gmail.com>
2014-05-02 12:03:24 -0300

    added image

--- ckanext/turing_theme/plugin.py
+++ ckanext/turing_theme/plugin.py
@@ -6,6 +6,7 @@ class TuringThemePlugin(plugins.SingletonPlugin):
 
 	'''
 	plugins.implements(plugins.IConfigurer)
-	
+
 	def update_config(self, config):
 		toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
